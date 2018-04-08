5ce8a6acc52b612d84391e4e85007ceea5a3d4dc
Maddy Dannenberg <mmdberg@gmail.com>
2018-04-08 07:11:47 -0600

    Hook up to redux dev tools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { rootReducer } from './reducers';
 
-const store = createStore(rootReducer)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools)
 
 ReactDOM.render(
   <Provider store={store}>
