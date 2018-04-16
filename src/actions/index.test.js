68a2447cc5a1cba65256c2e2c22f2417b843c890
Maddy Dannenberg <mmdberg@gmail.com>
2018-04-16 12:10:33 -0600

    Update actions and reducer test for searchRace

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,5 +1,5 @@
 import * as actions from './index';
-import { mockUser, mockCompletedRace } from '../mocks';
+import { mockUser, mockRace, mockCompletedRace } from '../mocks';
 
 describe('actions', () => {
   it('should return action type of CAPTURE_USER', () => {
@@ -34,5 +34,28 @@ describe('actions', () => {
       race
     };
     expect(actions.removeRace(race)).toEqual(expected);
+  });
+
+  it('should return action type of CLEAR_RACES', () => {
+    const expected = {
+      type: 'CLEAR_RACES'
+    }
+    expect(actions.clearRaces()).toEqual(expected)
+  });
+
+  it('should return action type of ADD_SEARCH_RACE', () => {
+    const race = mockRace;
+    const expected = {
+      type: 'ADD_SEARCH_RACE',
+      race
+    };
+    expect(actions.addSearchRace(race)).toEqual(expected);
+  });
+
+  it('should return action type of CLEAR_SEARCH_RACE', () => {
+    const expected = {
+      type: 'CLEAR_SEARCH_RACE'
+    }
+    expect(actions.clearSearchRace()).toEqual(expected)
   });
 });
